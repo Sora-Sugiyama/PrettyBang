942ab5314d0ed5755bf92f37d00b1f4224572440
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-30 13:01:14 +0900

    Create 1125.cpp

---
+++ 1125.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+string po10[13]={"","0","00","000","0000","00000"};
+int main(){
+    //ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int a,b,c;scanf("%d:%d:%d",&a,&b,&c);
+    printf("%d",b);
+    return 0;
+}
