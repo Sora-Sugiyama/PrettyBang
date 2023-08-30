13001c78bc53e821dcb92db7339b549e5c785e6c
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-30 12:23:19 +0900

    Create 1109.cpp

---
+++ 1109.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    cout<<"#include <stdio.h>\nint main()\n{\n  printf(\"Hello World\\n\");\n}";
+    return 0;
+}
