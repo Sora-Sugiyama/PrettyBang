ebe93b6a90ae4602d23b41a479471733a17ba156
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-30 12:56:51 +0900

    Create 1115.cpp

---
+++ 1115.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    cout<<fixed;
+    cout.precision(2);
+    float s;cin>>s;cout<<s;
+    return 0;
+}
