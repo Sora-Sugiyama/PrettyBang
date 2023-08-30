fb072a538501e1ee2c896100412d38854e491506
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-30 12:59:35 +0900

    Create 1122.cpp

---
+++ 1122.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    //ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int a,b;
+    scanf("%d.%d",&a,&b);
+    printf("%d\n%d",a,b);
+    return 0;
+}
