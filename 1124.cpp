12e8f7ea1c68ad893440a39209c0de8fb41a6d79
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-30 13:00:37 +0900

    Update 1124.cpp

--- 1124.cpp
+++ 1124.cpp
@@ -1,1 +1,13 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+string po10[13]={"","0","00","000","0000","00000"};
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    string s;cin>>s;
+    int n=1;
+    for(char x:s){
+        cout<<"["<<x<<po10[s.size()-n]<<"]\n";
+        n++;
+    }
+    return 0;
+}
