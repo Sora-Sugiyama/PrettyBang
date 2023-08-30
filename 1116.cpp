48b57fc17dc11b597d99de465752dbed52662a71
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-30 12:57:17 +0900

    Create 1116.cpp

---
+++ 1116.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    cout<<fixed;
+    cout.precision(2);
+    int a;cin>>a;
+    cout<<a<<" "<<a<<" "<<a;
+    return 0;
+}
