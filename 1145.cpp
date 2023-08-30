90a9595649c9de1fc63833fc457a6f0282f9c444
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-30 15:03:19 +0900

    Create 1145.cpp

---
+++ 1145.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    long long a,b,c;cin>>a>>b>>c;
+    cout<<fixed;
+    cout.precision(1);
+    cout<<a+b+c<<"\n"<<double(a+b+c)/3.0;
+    return 0;
+}
