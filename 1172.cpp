0f923443e961d7636c5a7ecc17aff3a1db543133
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-31 14:47:47 +0900

    Create 1172.cpp

---
+++ 1172.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	while(1){
+		int a;cin>>a;
+		if(!a)break;
+		cout<<a<<"\n";
+	}
+	return 0;
+}
