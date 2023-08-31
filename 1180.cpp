d28b6888ceaa369b4187bc3f7558ecc9725a13e6
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-31 15:44:19 +0900

    Create 1180.cpp

---
+++ 1180.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	int n,m;cin>>n>>m;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++)cout<<i<<" "<<j<<"\n";
+	}
+	return 0;
+}
