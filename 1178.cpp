9174beee3c956668f8fa17dfd429c53d84bec2ed
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-31 15:41:24 +0900

    Create 1178.cpp

---
+++ 1178.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    while(1){
+        char c;cin>>c;
+        cout<<c<<"\n";
+        if(c=='q')break;
+    }
+    return 0;
+}
