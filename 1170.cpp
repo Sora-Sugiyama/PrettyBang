3560875dba62c3e35eb734cb43e0fa82f3d96fa2
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-31 14:09:04 +0900

    Create 1170.cpp

---
+++ 1170.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+	t:
+	int n;cin>>n;
+	if(n){
+		cout<<n<<"\n";
+		goto t;
+	}
+	return 0;
+}
