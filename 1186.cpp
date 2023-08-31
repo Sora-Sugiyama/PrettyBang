fa707d6aba6f66b1c19f8c167437cfd7b352d49a
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-08-31 16:09:59 +0900

    Create 1186.cpp

---
+++ 1186.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);
+    int n;cin>>n;
+    n=int(sqrt(1+8*n)+(int(sqrt(1+8*n))!=sqrt(1+8*n)))/2;
+    cout<<n*(n+1)/2;
+    return 0;
+}
