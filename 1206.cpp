663f894a0fb0133d0a2760314344f90ef1f8636b
Sora Sugiyama <129280667+Sora-Sugiyama@users.noreply.github.com>
2023-09-08 14:10:10 +0900

    Create 1206.cpp

---
+++ 1206.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int A[303][303];
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(NULL);
+	int n,m;cin>>n>>m;
+	int cnt=0;
+	for(int i=1;i<=n;i++){
+		for(int j=1;j<=m;j++){
+			A[i][j]=++cnt;
+		}
+	}
+	for(int i=n;i>0;i--){
+		for(int j=1;j<=m;j++)cout<<A[i][j]<<" ";
+		cout<<"\n";
+	}
+	return 0;
+}
